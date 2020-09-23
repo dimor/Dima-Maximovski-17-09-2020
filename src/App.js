934320c756d62f60d7de984819a53db9a248e433
Dima Max <dimorm@gmail.com>
2020-09-23 14:10:50 +0300

    nav

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Layout from './containers/Layout/Layout';
 import Home from './containers/Home/Home';
 import Favorites from './containers/Favorites/Favorites';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Layout>
-      <BrowserRouter basename='/Dima-Maximovski-17-09-2020'>
+      <HashRouter basename='/Dima-Maximovski-17-09-2020'>
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/favorites" component={Favorites} />
         </Switch>
-        </BrowserRouter>
+        </HashRouter>
       </Layout>
 
     </div>
