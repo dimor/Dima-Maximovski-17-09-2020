bf3247136f0e66284e69977dc760f1082a6e2fe5
Dima Max <dimorm@gmail.com>
2020-09-23 11:50:58 +0300

    Updates

---
+++ precache-manifest.f3ae8fa03228e5fe2dd86ee8721aa89e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252b0cfaa5143e17bfe1bb8eb8cf74c4",
+    "url": "/Dima-Maximovski-17-09-2020/index.html"
+  },
+  {
+    "revision": "9a20dd18afed2a6883f2",
+    "url": "/Dima-Maximovski-17-09-2020/static/css/main.5de35e8d.chunk.css"
+  },
+  {
+    "revision": "1be48f5ff22700757e57",
+    "url": "/Dima-Maximovski-17-09-2020/static/js/2.8c4db183.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Dima-Maximovski-17-09-2020/static/js/2.8c4db183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a20dd18afed2a6883f2",
+    "url": "/Dima-Maximovski-17-09-2020/static/js/main.44a40d7b.chunk.js"
+  },
+  {
+    "revision": "892aa9b53efc19c8142f",
+    "url": "/Dima-Maximovski-17-09-2020/static/js/runtime-main.a700c0ab.js"
+  },
+  {
+    "revision": "fd14cef72a75c47e5a400d2f4bb7e91c",
+    "url": "/Dima-Maximovski-17-09-2020/static/media/search_icon.fd14cef7.png"
+  }
+]);
