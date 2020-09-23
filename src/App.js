eee87258af1887d83142e99412c472155cdc8673
Dima Max <dimorm@gmail.com>
2020-09-23 11:57:28 +0300

    fix router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import Layout from './containers/Layout/Layout';
 import Home from './containers/Home/Home';
 import Favorites from './containers/Favorites/Favorites';
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Layout>
+      <HashRouter basename='/'>
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/favorites" component={Favorites} />
         </Switch>
+        </HashRouter>
       </Layout>
 
     </div>
